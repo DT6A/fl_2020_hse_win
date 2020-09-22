7a08fc7be23fefc56ab0248d0a93d79e407102df
Тарасов Денис <dt6cgsg@gmail.com>
2020-09-22 22:15:35 +0300

    Expand tests

--- HW/tests.py
+++ HW/tests.py
@@ -54,7 +54,13 @@ r9 = Star(Alt(a, Alt(b, c)))
 # a?
 r10 = Alt(a, Epsilon())
 
+# (ab)*
+r11 = Star(Seq(a, b))
 
+# (abc)*
+r12 = Star(Seq(Seq(a, b), c))
+
+print("Correctness tests")
 profile_test(r0, '', '\epsilon', True)
 profile_test(r0, 'a', '\epsilon', False)
 
@@ -69,7 +75,8 @@ profile_test(r10, 'a', 'a?', True)
 profile_test(r10, 'b', 'a?', False)
 
 s_list = ["b", "c", "d", "ba", "aaaa", "c" + "abba" * 25, "c" + "abba" * 50, "c" + "abba" * 100]
-for s in s_list: profile_test(r3, s, 'b | (c (a|b)*)')
+for s in s_list:
+    profile_test(r3, s, 'b | (c (a|b)*)')
 
 profile_test(r3, "cabba", 'b | (c (a|b)*)', True)
 profile_test(r3, "cabba" * 625, 'b | (c (a|b)*)', False)
@@ -79,18 +86,24 @@ profile_test(r4, 'a' * 50 + 'b', '(a|a)*a(a|a)*', False)
 profile_test(r4, 'a', '(a|a)*a(a|a)*', True)
 profile_test(r4, '', '(a|a)*a(a|a)*', False)
 
-print('\nString length dependency')
+print('\nComplexity length dependency')
 for sl in range(25, 151, 25):
     profile_test(r1, 'a' * sl, 'a*a', True)
     profile_test(r4, 'a' * sl, '(a|a)*a(a|a)*', True)
 
-print('\nString length dependency 2')
+print('\nComplexity length dependency 2')
 for sl in range(25, 76, 25):
     profile_test(r5, 'ab' * sl, '(a|b)*(a|b)', True)
     profile_test(r6, 'ab' * sl, '(a|b)*(a|b)(a|b)*', True)
 
-print('\nComplexity dependency')
+print('\nALT complexity dependency')
 for sl in range(50, 151, 25):
     profile_test(r7, 'aa' * sl, 'a*', True)
     profile_test(r8, 'aa' * sl, '(a|b)*', True)
     profile_test(r9, 'aa' * sl, '(a|b|c)*', True)
+
+print('\nSEQ complexity dependency')
+for sl in range(25, 126, 25):
+    profile_test(r7, 'a' * (sl * 3), 'a*', True)
+    profile_test(r11, 'ab' * int(sl * 1.5), '(ab)*', True)
+    profile_test(r12, 'abc' * sl, '(abc)*', True)
