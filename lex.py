650eecc82179afd2f6a99a0298a6422dfe9aea12
Тарасов Денис <dt6cgsg@gmail.com>
2020-10-04 21:55:11 +0300

    Complete task

--- lex.py
+++ lex.py
@@ -1,54 +1,93 @@
-import ply.lex as lex 
-import sys
+import ply.lex as lex
 
-reserved = {
-  'if': 'IF', 
-  'then': 'THEN',
-  'else': 'ELSE'
-}
 
 tokens = [
-  'NUM', 
-  'PLUS', 
-  'MULT',
+  'CORK',
   'ID',
-  'LBR',
-  'RBR'
-] + list(reserved.values())
+  'DELIM',
+  'AND',
+  'OR',
+  'OBR',
+  'CBR'
+]
+
+t2w_d = {
+    'CORK': ':-',
+    'ID': 'id',
+    'DELIM': '.',
+    'OBR': '(',
+    'CBR': ')'
+}
+
+def t2w(t):
+    if t in t2w_d:
+        return t2w_d[t]
+    return t
 
 def t_ID(t):
-  r'[a-z_][a-z_0-9]*'
-  t.type = reserved.get(t.value, 'ID')
+  r'[a-zA-Z_][a-zA-Z_0-9]*'
   return t
 
-def t_NUM(t): 
-  r'[0-9]+'
-  t.value = int(t.value)
-  return t
 
-t_PLUS = r'\+'
-t_MULT = r'\*'
-t_LBR = r'\('
-t_RBR = r'\)'
+t_CORK = r'\:\-'
+t_DELIM = r'\.'
+t_AND = r'\,'
+t_OR = r'\;'
+t_OBR = r'\('
+t_CBR = r'\)'
 
 t_ignore = ' \t'
 
-def t_newline(t): 
+
+def update_lex_pos(tok):
+  if tok.lineno != update_lex_pos.prev_line:
+    update_lex_pos.pos_subst = update_lex_pos.prev_tok_pos
+    update_lex_pos.prev_line = tok.lineno
+  update_lex_pos.prev_tok_pos = tok.lexpos + len(str(tok.value))
+update_lex_pos.prev_tok_pos = 0
+update_lex_pos.pos_subst = 0
+update_lex_pos.prev_line = 1
+
+
+def t_newline(t):
   r'\n+'
   t.lexer.lineno += len(t.value)
+  update_lex_pos(t)
 
-def t_error(t): 
-  print("Illegal character '%s'" % t.value[0])
+
+def t_error(t):
+  t.value = t.value[0]
+  update_lex_pos(t)
+  print("Illegal character '%s' at line %i pos %i" % (t.value, t.lineno, t.lexpos - update_lex_pos.pos_subst))
   t.lexer.skip(1)
+  tokenize_file.no_errs = False
 
-lexer = lex.lex() 
 
-#lexer.input(sys.argv[1]) 
+def tokenize_file(file_name):
+  update_lex_pos.prev_tok_pos = 0
+  update_lex_pos.pos_subst = 0
+  update_lex_pos.prev_line = 1
 
-#while True: 
-#  r'\+' 
-#  13
-#  tok = lexer.token() 
-#  if not tok: 
-#    break
-#  print(tok)
+  lexer = lex.lex()
+  tokens = []
+  tokenize_file.no_errs = True
+
+  with open(file_name, 'r') as inf:
+    lexer.input(inf.read())
+
+  while True:
+    tok = lexer.token()
+    if not tok:
+      break
+    update_lex_pos(tok)
+    tok.lexpos -= update_lex_pos.pos_subst
+    tokens.append(tok)
+    #print(', '.join(list(map(str,[tok.type, tok.value, tok.lineno, tok.lexpos]))))
+
+  #print(tokens, tokenize_file.no_errs)
+  return tokenize_file.no_errs, tokens
+
+
+lexer = lex.lex()
+
+#tokenize_file(sys.argv[1])
--- parse.py
+++ parse.py
@@ -1,58 +1,97 @@
-import ply.yacc as yacc 
+import sys  # import sys module
 
-from lex import tokens 
+import ply.yacc as yacc  # import yacc
 
-# if 42 then 13 else 0
-# if 42 then if 1+2 then 6 else 3*5 
+from lex import tokens  # import tokens from lex file
 
-def p_if(p): 
-  '''if : IF expression THEN if ELSE if
-        | IF expression THEN if 
-        | expression '''
-  if len(p) == 7: 
-    p[0] = p[4] if p[2] == 0 else p[6]
-  else: 
-    if len(p) == 5: 
-      p[0] = p[4] if p[2] == 0 else 42
-    else: 
-      p[0] = p[1]
-  
 
-def p_expression_plus(p): 
-  'expression : expression PLUS term'
-  p[0] = p[1] + p[3]
+class Node: # AST node
+  def __init__(self, left, right, name):  # initialization function
+    self.left = left  # set left child
+    self.right = right  # set right child
+    self.name = name  # set node name
 
-def p_expression_term(p): 
-  'expression : term' 
-  p[0] = p[1]
 
-def p_term_times(p): 
-  'term : term MULT factor'
-  p[0] = p[1] * p[3]
+def pr(node):  # print AST
+  a = node.name  # initialize string with node name
+  if node.left != None:  # check if left child exists
+    a += '(' + pr(node.left) + ')'  # get left child representation and add to the current one
+  if node.right != None:  # check if right child exists
+    a += '(' + pr(node.right) + ')'  # get right child representation and add to the current one
+  return a  # return string
 
-def p_term_factor(p): 
-  'term : factor' 
-  p[0] = p[1] 
 
-def p_factor_num(p): 
-  'factor : NUM'
-  p[0] = p[1]
+def p_defs_seq(p):  # parse sequence of definitions
+  'seq : def DELIM seq'
+  p[0] = Node(p[1], p[3], 'SEQ')  # create sequence node
 
-def p_factor_expr(p): 
-  'factor : LBR expression RBR'
-  p[0] = p[2]
+def p_seq_def(p):  # parse the last definition
+  'seq : def DELIM'
+  p[0] = p[1]  # pass it through
 
-def p_error(p): 
-  print("Syntax error")
+def p_def_full(p):  # parse definition with head and body
+  'def : atom CORK disj'
+  p[0] = Node(p[1], p[3], 'DEF')  # create definition node
 
-parser = yacc.yacc()
+def p_def_head(p):  # parse definition without body
+  'def : atom'
+  p[0] = Node(p[1], None, 'DEF')  # create definition node
 
-while True: 
-  try: 
-    s = input("calc> ")
-  except EOFError: 
-    break
-  if not s: 
-    continue
-  result=parser.parse(s)
-  print(result) 
+def p_disj_disj(p):  # parse disjunction
+  'disj : conj OR disj'
+  p[0] = Node(p[1], p[3], 'OR')  # create disjunction node
+
+def p_disj_conj(p):  # parse disjunction as conjunction
+  'disj : conj'
+  p[0] = p[1]  # pass it through
+
+def p_conj_conj(p):  # parse conjunction
+  'conj : expr AND conj'
+  p[0] = Node(p[1], p[3], 'AND')  # create conjunction
+
+def p_conj_atom(p):  # parse conjunction as expression
+  'conj : expr'
+  p[0] = p[1]  # pass it through
+
+def p_expr_disj(p):  # parse expression with brackets
+  'expr : OBR disj CBR'
+  p[0] = p[2]  # pass disjunction from inside through
+
+def p_expr_atom(p):  # parse expression as atom
+  'expr : atom'
+  p[0] = p[1]  # pass it through
+
+
+def p_atom_aseq(p):  # parse all atoms possible usages
+  '''atom : ID atom
+          | OBR atom CBR atom
+          | OBR atom CBR
+          | ID '''
+  if len(p) == 3:  # sequence of ID and atom
+    p[0] = Node(Node(None, None, 'ID ' + p[1]), p[2], 'ATOMSEQ')  # create atom sequence node with ID node inside
+  elif len(p) == 4:  # last atom at subexpression inside of the brackets
+    p[0] = p[2]  # extract atom from brackets
+  elif len(p) == 5:  # sequence of atoms where the first one is inside of the brackets
+    p[0] = Node(p[2], p[4], 'ATOMSEQ')  # create node of atoms sequence extracting the first one
+  else:  # last ID at subexpression
+    p[0] = Node(None, None, 'ID ' + p[1])  # create ID node
+
+
+def p_error(p):  # process parsing error
+  if p == None:  # p is the last token
+    print("Missing dot at the end of the definition")  # print error message
+  else:  # p is not at the end
+    print("Expected something different at line", p.lineno, 'col', p.lexpos)  # print error message
+  raise ValueError("Syntax error")  # raise exception to stop parsing
+
+
+sys.stdout = open(sys.argv[1] + '.out', 'w')  # redirect standart output to the file
+
+parser = yacc.yacc()  # create parser
+
+with open(sys.argv[1], 'r') as inf:  # open input file
+  try:  # try to parse file
+    result = parser.parse(inf.read())  # get file content and process it
+    print(pr(result))  # print AST
+  except ValueError:  # catch exception
+    pass  # ignore it
